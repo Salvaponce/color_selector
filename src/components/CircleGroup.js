60693b02eebc93b8ca51131776b19f37ceafa1a1
Salvador Ponce <salvaponce92@gmail.com>
2022-12-18 20:34:44 +0100

    fixing delete button

--- src/components/CircleGroup.js
+++ src/components/CircleGroup.js
@@ -11,17 +11,19 @@ function CircleGroup({id, colors, name , selected, removeGroup}){
 
   if(colors){
     return(
-    <>    
+    <div className="circleGroupContainer">    
       <div className="not-pointer">  
         <h3>{name}</h3>
         <Circle isSelected={selected} color={colors[0]}/>  
         <Circle isSelected={selected} color={colors[1]}/>
         <Circle isSelected={selected} color={colors[2]}/>
         <Circle isSelected={selected} color={colors[3]}/>
-        <Circle isSelected={selected} color={colors[4]}/>
+        <Circle isSelected={selected} color={colors[4]}/>        
+      </div>
+      <div>
         <AiFillCloseCircle onClick={() => removeGroup(id)}/>
       </div>
-    </>
+    </div>
   )
   }
   
